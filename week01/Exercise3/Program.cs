45f2de6250e62e2241946a5d60cd5c71d5343f21
novyboy <novakaiisland+github@gmail.com>
2026-05-08 16:34:09 +0900

    excercise3 with yes

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,24 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1, 100);
-        
-        Console.WriteLine("Guess the Magic Number!");
-        
-        int numberOfGuesses = 0;
-        int guess = 0;
+        string playAgain = "yes";
         do {
-            Console.Write("Your guess is: ");
-            string userInput = Console.ReadLine() ?? "";
-            guess = int.Parse(userInput);
-            if (guess < magicNumber) {
-                Console.WriteLine("Higher");
-            } else if(guess > magicNumber) {
-                Console.WriteLine("Lower");
-            }
-            numberOfGuesses++;
-        } while (guess != magicNumber);
-        Console.WriteLine($"Correct! You guessed the magic number in {numberOfGuesses} guesses");
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+            
+            Console.WriteLine("Guess the Magic Number!");
+            
+            int numberOfGuesses = 0;
+            int guess = 0;
+            do {
+                Console.Write("Your guess is: ");
+                string userInput = Console.ReadLine() ?? "";
+                guess = int.Parse(userInput);
+                if (guess < magicNumber) {
+                    Console.WriteLine("Higher");
+                } else if(guess > magicNumber) {
+                    Console.WriteLine("Lower");
+                }
+                numberOfGuesses++;
+            } while (guess != magicNumber);
+            Console.WriteLine($"Correct! You guessed the magic number in {numberOfGuesses} guesses");
+            
+            Console.Write("Play again? ");
+            playAgain = Console.ReadLine() ?? "no";
+        } while (playAgain.ToLower() == "yes");
     }
 }
