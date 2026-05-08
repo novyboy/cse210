92bf27fecdc1fa2846aae46e6647e451fb168ebd
novyboy <novakaiisland+github@gmail.com>
2026-05-08 16:29:47 +0900

    program3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        
+        Console.WriteLine("Guess the Magic Number!");
+        
+        int numberOfGuesses = 0;
+        int guess = 0;
+        do {
+            Console.Write("Your guess is: ");
+            string userInput = Console.ReadLine() ?? "";
+            guess = int.Parse(userInput);
+            if (guess < magicNumber) {
+                Console.WriteLine("Higher");
+            } else if(guess > magicNumber) {
+                Console.WriteLine("Lower");
+            }
+            numberOfGuesses++;
+        } while (guess != magicNumber);
+        Console.WriteLine($"Correct! You guessed the magic number in {numberOfGuesses} guesses");
     }
 }
