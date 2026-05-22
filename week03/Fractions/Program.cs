f1a4db7f566d3216144d1122c33f04a899adc30b
novyboy <novakaiisland+github@gmail.com>
2026-05-22 15:21:20 +0900

    fraction

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction one = new Fraction();
+        Fraction wholeNumber = new Fraction(5);
+        Fraction fraction = new Fraction(3,4);
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+        Console.WriteLine(wholeNumber.GetFractionString());
+        Console.WriteLine(wholeNumber.GetDecimalValue());
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
     }
 }
